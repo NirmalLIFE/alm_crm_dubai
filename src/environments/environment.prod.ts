8ac35c3fc7513cb93252ffacf3ac18d5a65ea7d1
NirmalLIFE <nirmaltrejilal@gmail.com>
2024-11-30 15:35:24 +0530

    header change in dubai,dashboard of customer analysis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     production: true,
     base_url: 'http://localhost:8081/',
     base_img_url: 'http://localhost:8081/uploads/',
-    yeastar_url: 'https://almaragy.ras.yeastar.com/openapi/v1.0/',
+    yeastar_url: 'https://almaraghidxb.ras.yeastar.com/openapi/v1.0/',
     firebase: {
         apiKey: 'AIzaSyBBKv_LdCNFhr4IMr9Msq5NXLU_lvx2mxY',
         authDomain: 'maraghi-crm.firebaseapp.com',
@@ -17,8 +17,8 @@ export const environment = {
         appId: '1:1062000739352:web:fd5d6e73bfca0d90752f6d',
         vapidKey: 'BEv7DNp4_RMRe7LLtjiFCiOYtVmkGQ5Yj_TO9Gvwgd49hQIhv-YELG2RcZmfq9gG5X0LkAx88xSwBgP0MOtQBGU',
     },
-    //SOCKET_ENDPOINT: "https://chatramsserver-production-e8fc.up.railway.app/"
     SOCKET_ENDPOINT: 'http://localhost:3000/',
+    //SOCKET_ENDPOINT: "https://chatramsserver-production.up.railway.app/"
 };
 
 // export const environment = {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,21 +9,21 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-    production: true,
-    base_url: 'http://localhost:8081/',
-    base_img_url: 'http://localhost:8081/uploads/',
-    yeastar_url: 'https://almaragy.ras.yeastar.com/openapi/v1.0/',
-    firebase: {
-        apiKey: 'AIzaSyBBKv_LdCNFhr4IMr9Msq5NXLU_lvx2mxY',
-        authDomain: 'maraghi-crm.firebaseapp.com',
-        projectId: 'maraghi-crm',
-        storageBucket: 'maraghi-crm.appspot.com',
-        messagingSenderId: '1062000739352',
-        appId: '1:1062000739352:web:fd5d6e73bfca0d90752f6d',
-        vapidKey: 'BEv7DNp4_RMRe7LLtjiFCiOYtVmkGQ5Yj_TO9Gvwgd49hQIhv-YELG2RcZmfq9gG5X0LkAx88xSwBgP0MOtQBGU',
-    },
-    //SOCKET_ENDPOINT: "https://chatramsserver-production-e8fc.up.railway.app/"
-    SOCKET_ENDPOINT: 'http://localhost:3000/',
+  production: true,
+  base_url: 'http://localhost:8081/',
+  base_img_url: 'http://localhost:8081/uploads/',
+  yeastar_url: 'https://almaraghidxb.ras.yeastar.com/openapi/v1.0/',
+  firebase: {
+      apiKey: 'AIzaSyBBKv_LdCNFhr4IMr9Msq5NXLU_lvx2mxY',
+      authDomain: 'maraghi-crm.firebaseapp.com',
+      projectId: 'maraghi-crm',
+      storageBucket: 'maraghi-crm.appspot.com',
+      messagingSenderId: '1062000739352',
+      appId: '1:1062000739352:web:fd5d6e73bfca0d90752f6d',
+      vapidKey: 'BEv7DNp4_RMRe7LLtjiFCiOYtVmkGQ5Yj_TO9Gvwgd49hQIhv-YELG2RcZmfq9gG5X0LkAx88xSwBgP0MOtQBGU',
+  },
+  SOCKET_ENDPOINT: 'http://localhost:3000/',
+  //SOCKET_ENDPOINT: "https://chatramsserver-production.up.railway.app/"
 };
 
 // export const environment = {
